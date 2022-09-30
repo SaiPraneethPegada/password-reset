cb0ffec3911972cecc97848a51bdffedd1d693df
Pegada Sai Praneeth <saipraneethpegada@gmail.com>
2022-09-30 15:46:41 +0530

    server_url updated

--- src/App.js
+++ src/App.js
@@ -6,25 +6,27 @@ import Signup from "./components/Signup";
 import Activate from "./components/Activate";
 import Forgot from "./components/Forgot";
 import Reset from "./components/Reset";
-export const url = "http://localhost:8000";
-
+export const url = "https://password--reset--backend.herokuapp.com/";
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/activate/:activation_token" element={<Activate />} />
-          <Route path="/forgot_password" element={<Forgot />} />
-          <Route path="/reset_password/:token" element={<Reset />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="*" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route
+                        path="/activate/:activation_token"
+                        element={<Activate />}
+                    />
+                    <Route path="/forgot_password" element={<Forgot />} />
+                    <Route path="/reset_password/:token" element={<Reset />} />
+                    <Route path="/home" element={<Home />} />
+                    <Route path="*" element={<Login />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
